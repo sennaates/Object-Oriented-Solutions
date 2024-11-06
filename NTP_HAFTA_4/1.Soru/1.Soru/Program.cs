02b6d103f8cc4ace83d406fbe04c40970a847207
Sena Ateş <147206070+Sena-ates@users.noreply.github.com>
2024-11-06 23:17:23 +0300

    Add files via upload

---
+++ NTP_HAFTA_4/1.Soru/1.Soru/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Soru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankaHesabi hesap1 = new BankaHesabi("5218", 8888888);
+
+            hesap1.ParaYatir(1);
+            hesap1.ParaCek(2);
+
+            Console.ReadLine();
+        }
+    }
+    public class BankaHesabi
+    {
+        public string HesapNumarasi { get; set; }
+        public decimal Bakiye { get; private set; }
+        public BankaHesabi(string hesapNumarasi, decimal ilkBakiye)
+        {
+            HesapNumarasi = hesapNumarasi;
+            Bakiye = ilkBakiye;
+        }
+        public void ParaYatir(decimal miktar)
+        {
+            if (miktar > 0)
+            {
+                Bakiye += miktar;
+                Console.WriteLine($"{miktar} TL yatırdiniz. Hesap bakiyesi: {Bakiye} TL");
+            }
+            else
+            {
+                Console.WriteLine("Yatıracağınız miktar negatif olamaz.");
+            }
+        }
+        public void ParaCek(decimal miktar)
+        {
+            if (miktar > 0 && miktar <= Bakiye)
+            {
+                Bakiye -= miktar;
+                Console.WriteLine($"{miktar} TL çekildi. Güncel bakiye: {Bakiye} TL");
+            }
+            else if (miktar > Bakiye)
+            {
+                Console.WriteLine("Yetersiz bakiye!");
+            }
+            else
+            {
+                Console.WriteLine("Çekeceğiniz miktar negatif olamaz.");
+            }
+
+        }
+    }
+}
