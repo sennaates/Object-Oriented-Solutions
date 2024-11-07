99589830fb42b9aea0515dacfe798f3f54a53bea
Sena Ateş <147206070+Sena-ates@users.noreply.github.com>
2024-11-07 13:36:38 +0300

    Add files via upload

---
+++ NTP_HAFTA_4/4.Soru/4.Soru/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4.Soru
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kisi kisi1 = new Kisi("Enes", "Berat", "5378769065");
+            kisi1.KisiBilgisi();
+            Console.ReadLine();
+        }
+    }
+    class Kisi
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public string TelefonNumarasi { get; set; }
+
+        public Kisi(string ad, string soyad, string telefonNumarasi)
+        {
+            Ad = ad;
+            Soyad = soyad;
+            TelefonNumarasi = telefonNumarasi;
+        }
+
+        public void KisiBilgisi()
+        {
+            Console.WriteLine("Ad: " + Ad);
+            Console.WriteLine("Soyad: " + Soyad);
+            Console.WriteLine("Telefon Numarasi: " + TelefonNumarasi);
+        }
+    }
+}
