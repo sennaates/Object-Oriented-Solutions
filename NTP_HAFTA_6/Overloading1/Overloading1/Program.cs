654d909c02c0137d27eeb5b02ca8725e6df47a09
Sena Ateş <147206070+Sena-ates@users.noreply.github.com>
2024-11-27 23:24:16 +0300

    Add files via upload

---
+++ NTP_HAFTA_6/Overloading1/Overloading1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading1
+{
+    /*
+    1.Matematiksel İşlemleri Çeşitlendiren Bir Fonksiyon
+    Bir fonksiyon yazın:
+•	Aynı adla ama farklı parametrelerle toplam işlemi yapacak.
+•	İlk sürümü iki tam sayıyı toplasın.
+•	İkinci sürümü üç tam sayıyı toplasın.
+•	Üçüncü sürümü bir dizi (array) tam sayıyı toplasın.
+    */
+
+
+    class Program
+    {
+        static int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        static int Topla(int sayi1, int sayi2, int sayi3 )
+        {
+            return sayi1 + sayi2+ sayi3;
+        }
+        static int Topla(int[] array)
+        {
+            int sonuc = 0;
+            foreach(int sayi in array)
+            {
+                sonuc += sayi;    
+            }
+            return sonuc;
+        }
+        
+        
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 3, 4, 5, 6, 7 };
+
+            int ikiSayininToplami = Topla(3, 4);
+            int ucSayininToplami = Topla(2, 6, 8);
+            int diziElemanlariToplami = Topla(sayilar);
+
+            Console.WriteLine(ikiSayininToplami);
+            Console.WriteLine(ucSayininToplami);
+            Console.WriteLine(diziElemanlariToplami);
+
+            Console.Read();
+        }
+    }
+}
---
+++ NTP_HAFTA_6/Overloading2/Overloading2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overloading2
+{
+    /*Farklı Şekillerin Alanını Hesaplayan Bir Fonksiyon
+    //Bir fonksiyon yazın:
+    //-Aynı adla ama farklı parametrelerle farklı şekillerin alanını hesaplasın.
+    //-İlk sürüm, bir karenin alanını hesaplasın(bir kenar uzunluğu verilerek).
+    //-İkinci sürüm, bir dikdörtgenin alanını hesaplasın(iki kenar uzunluğu verilerek).
+    //-Üçüncü sürüm, bir dairenin alanını hesaplasın(yarıçap verilerek).
+    */
+    class Program
+
+
+    {
+
+        static int AlanHesapla(int kenarUzunlugu)
+        {
+            return kenarUzunlugu * kenarUzunlugu;
+        }
+        static double AlanHesapla(double kisaKenar, double uzunKenar)
+        {
+            return kisaKenar * uzunKenar;
+        }
+        static double AlanHesapla(double yaricap)
+        {
+            return Math.PI * yaricap * yaricap;
+        }
+        static void Main(string[] args)
+        {
+            int AlanHesapla(int kenarUzunlugu)
+            {
+                return kenarUzunlugu * kenarUzunlugu;
+            }
+            double AlanHesapla(double kisaKenar, double uzunKenar)
+            {
+                return kisaKenar * uzunKenar;
+            }
+            double AlanHesapla(double yaricap)
+            {
+                return Math.PI * yaricap * yaricap;
+            }
+        }
+    }
+}
